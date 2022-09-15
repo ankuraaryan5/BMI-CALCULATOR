55b3cbd409aa4649926b36d7ae041772a7ed85d0
ankuraaryan5 <86702313+ankuraaryan5@users.noreply.github.com>
2022-09-15 21:46:35 +0530

    required files added

---
+++ script.js
@@ -0,0 +1,33 @@
+let btn = document.getElementById('calcbtn');
+
+btn.addEventListener('click', function(){
+    let weight = document.getElementById('weight-input').value;
+    let height = document.getElementById('height-input').value;
+    console.log(weight)
+    console.log(height)
+    let bmi = (weight/(height*height)*10000);
+    document.getElementById('bmires').value = bmi;
+})
+function myFunc() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
